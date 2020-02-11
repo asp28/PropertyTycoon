027b20e4aad13dd0f18173dd252af0f3341a3b7a
Christian Saraty <christians1@hotmail.co.uk>
2020-02-11 13:02:34 +0000

    updated Dice

--- PropertyTycoon/src/test/java/com/mycompany/propertytycoon/PlayerTest.java
+++ PropertyTycoon/src/test/java/com/mycompany/propertytycoon/PlayerTest.java
@@ -1,8 +1,10 @@
 package java.com.mycompany.propertytycoon;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PlayerTest {
@@ -12,7 +14,7 @@ public class PlayerTest {
      * Test that rolling the dice will cause the player to move to that location that it is not on;
      */
     @Test
-    public void testRollDice() {
+    public void testRollDice() throws IOException, InvalidFormatException {
         ArrayList<Integer> a = new ArrayList<Integer>();
         a.add(1);
         PropertyCards propertyCards = new PropertyCards("test", "green", "", true, 1000, "10", a, 100);
@@ -20,7 +22,7 @@ public class PlayerTest {
         properties.add(propertyCards);
         Bank bank = new Bank(properties);
         Dice dice = new Dice();
-        Board board = new Board(properties);
+        Board board = new Board();
         Player player = new Player(board, dice, bank);
 
         player.rollDice();
@@ -32,15 +34,15 @@ public class PlayerTest {
      * Test that the player will move around the board resetting the location back to 0 when it reaches the last known location
      */
     @Test
-    public void testRoundTheBoard() {
+    public void testRoundTheBoard() throws IOException, InvalidFormatException {
         ArrayList<Integer> a = new ArrayList<Integer>();
         a.add(1);
         PropertyCards propertyCards = new PropertyCards("test", "green", "", true, 1000, "10", a, 100);
         ArrayList<PropertyCards> properties = new ArrayList<PropertyCards>();
         properties.add(propertyCards);
-        Bank bank = new Bank(properties);
+        Bank bank = new Bank();
         Dice dice = new Dice();
-        Board board = new Board(properties);
+        Board board = new Board();
         Player player = new Player(board, dice, bank);
         player.setPlayerLocation(42);
         player.rollDice();
