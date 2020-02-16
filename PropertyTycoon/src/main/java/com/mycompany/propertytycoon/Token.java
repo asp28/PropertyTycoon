92e31a40f7ce6b9c7ccfce30595058350c32c03c
Aiden <agentpea2000@gmail.com>
2020-02-16 23:25:30 +0000

    Created Token class to add enum field 'token' to Player class

---
+++ PropertyTycoon/src/main/java/com/mycompany/propertytycoon/Token.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.propertytycoon;
+
+/**
+ *
+ * @author Aiden
+ * @author Big Nerd Notation
+ */
+
+public enum Token {
+    BOOT, PHONE, GOBLET, HATSTAND, CAT, SPOON
+}
