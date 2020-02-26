fbaacb39d500df48aa4516d9280e6b6f08b49919
Aiden <agentpea2000@gmail.com>
2020-02-26 21:12:16 +0000

    Created test class for Board

--- PropertyTycoon/src/test/java/com/mycompany/propertytycoon/BoardTest.java
+++ PropertyTycoon/src/test/java/com/mycompany/propertytycoon/BoardTest.java
@@ -1,43 +1,44 @@
 package com.mycompany.propertytycoon;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.junit.*;
-
 import java.io.IOException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+/**
+ *
+ * @author agent
+ */
 public class BoardTest {
-
-    Board board;
-
-    public BoardTest() throws IOException, InvalidFormatException {
-        board = new Board();
+    
+    private Board board;
+    
+    public BoardTest() {
     }
-
+    
     @BeforeClass
     public static void setUpClass() {
     }
-
+    
     @AfterClass
     public static void tearDownClass() {
     }
-
+    
     @Before
-    public void setUp() {
+    public void setUp() throws IOException, InvalidFormatException {
+        board = new Board();
     }
-
+    
     @After
     public void tearDown() {
     }
 
     @Test
-    public void getBoardLocations() {
-       // assertEquals("Go", board.getBoardLocations().get(0).getName());
+    public void boardLocationsTest(){
+        assertEquals(board.getBoardLocations().get(0).getClass().getSimpleName(), "GoPiece");
     }
-
 }
